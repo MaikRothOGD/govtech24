cf282c5fc5d7bff5db03a34aafcfeda62b88be2b
Benoit G. Regamey <61193520+benoitregamey@users.noreply.github.com>
2024-03-14 17:45:59 +0100

    Update ai1.py

--- opendatasearchapp/ai1/ai1.py
+++ opendatasearchapp/ai1/ai1.py
@@ -1,7 +1,25 @@
-class AI1:
-    def __init__(self):
-        print("ai1")
+from openai import OpenAI
 
-    def refine(self, keywords):
-        print(keywords)
-        return ["a", "b", "c"]
+client = OpenAI()
+
+class AI1:
+
+    def __init__(self):
+        pass
+
+    def refine(self, input: str) -> list:
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are an assistant to reformulate and improve search query for solr search platform. You must give the most relevant search keywords. You must give only 5 keywords separated by white space."
+                    },
+                {
+                    "role": "user", 
+                    "content": input
+                    }
+            ]
+        )
+
+        return completion.choices[0].message.content.split(" ")
