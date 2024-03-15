dc347b2905e2154f5a5ae6cdba9f1f96d64c8f6c
“benoitregamey” <benoitregamey@hotmail.com>
2024-03-15 11:03:38 +0100

    open CORS for all origin in backend, display alternative text when no results in UI

--- opendatasearchapp/webserver.py
+++ opendatasearchapp/webserver.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 from backend import Backend
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 backend = Backend()
 
